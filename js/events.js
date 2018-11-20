ba6466995fa66fb4a7b64efd26c48985753d9d7c
Mohammad Ali <ali.mohammed200@gmail.com>
2018-11-20 07:47:26 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -15,6 +15,11 @@ function pressIt(){
     }
   });
 }
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('Tour form is going to be submitted now.');
+  });
+}
 $(document).ready(function(){
   getIt();
   frameIt();
@@ -22,10 +27,6 @@ $(document).ready(function(){
 });
 
 /*
-+ Define a function `pressIt` that does not accept a parameter. The function
-should bind a `keydown` event to the input field of the form that alerts a user
-when they have pressed the `G` key.
-
 + Define a function `submitIt` that does not accept a parameter. The function
 should bind a `submit` event to the form that alerts `"Your form is going to be
 submitted now."`.
